362799cd96114451adea9eca3415059ba1b96f67
Lucas Favaro Borsatto <paradigma0621@gmail.com>
2023-01-31 11:22:13 -0300

    Simple access to backend that returns one String

--- app/js/controllers/person.controller.js
+++ app/js/controllers/person.controller.js
@@ -1,8 +1,13 @@
 define([], function () {
     'use strict';
 
-    var PersonController = function ($scope) {
-        $scope.personName = "João Pedro";
+    var PersonController = function ($scope, $http) {
+        const url = 'http://localhost:8080/sayHello';
+        $http.get(url).then(function success(response) {
+            $scope.personName = response.data;
+        }, function error(response) {
+            $scope.personName = 'ERROR';
+        });
     };
 
     return PersonController;
