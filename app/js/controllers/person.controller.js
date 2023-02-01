7d7e892e9dcec8aa44195154c1a32e906aa0c06b
Lucas Favaro Borsatto <paradigma0621@gmail.com>
2023-02-01 14:44:03 -0300

    Implemented POST request that creates a Person row in database

--- app/js/controllers/person.controller.js
+++ app/js/controllers/person.controller.js
@@ -1,14 +1,40 @@
 define([], function () {
     'use strict';
 
-    var PersonController = function ($scope, $http) {
-        const url = 'http://localhost:8080/sayHello';
-        $http.get(url).then(function success(response) {
-            $scope.personName = response.data;
-        }, function error(response) {
-            $scope.personName = 'ERROR';
-        });
-    };
+    return ['$scope', '$http',
+        function($scope, $http) {
+            $scope.retornaValor = function() {
+                const url = 'http://localhost:8080/sayHello';
+                $http.get(url).then(function success(response) {
+                    $scope.personName = response.data;
+                }, function error(response) {
+                    $scope.personName = 'ERROR';
+                });
+            };
+ 
 
-    return PersonController;
+            // Bellow used to send a POST to endpoint http://localhost:8080/persons/savePerson
+            $scope.name = null;
+            $scope.removed = null;
+            $scope.lblMsg = null;
+
+            $scope.postdata = function (name, removed) {
+                var data = {
+                    name: name,
+                    removed: removed
+                };
+            
+                //Call the services
+                $http.post('http://localhost:8080/persons/savePerson', JSON.stringify(data))
+                    .then(function (response) {
+                       if (response.data)
+                        $scope.msg = "Post Data Submitted Successfully!";
+                    }, function (response) {
+                        $scope.msg = "Service not Exists";
+                        $scope.statusval = response.status;
+                        $scope.statustext = response.statusText;
+                        $scope.headers = response.headers();
+                    });
+            };
+    }];
 });
